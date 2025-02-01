111252e2adc66fb3f76700975b6e3edd8b745a05
Rose Njambi <njambi.r@gmail.com>
2025-02-01 20:48:16 +0300

    edit4

--- task_5.sql
+++ task_5.sql
@@ -8,8 +8,8 @@ CREATE TABLE alx_book_store.Customer(
 );
 
 
-INSERT INTO Customer (customer_id,customer_name,email, address)
+INSERT INTO customer (customer_id,customer_name,email, address)
 VALUES (1,"Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave." )
 
-INSERT INTO Customers (customer_id,customer_name,email, address)
+INSERT INTO customers (customer_id,customer_name,email, address)
 VALUES (1,"Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave." )
