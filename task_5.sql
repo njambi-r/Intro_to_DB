53974c7a0cedf7e3c2878dabd6074e4077f41e8d
Rose Njambi <njambi.r@gmail.com>
2025-02-01 20:45:32 +0300

    edit3

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,15 @@
 USE alx_book_store;
+
+CREATE TABLE alx_book_store.Customer(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+);
+
+
+INSERT INTO Customer (customer_id,customer_name,email, address)
+VALUES (1,"Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave." )
+
 INSERT INTO Customers (customer_id,customer_name,email, address)
 VALUES (1,"Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave." )
